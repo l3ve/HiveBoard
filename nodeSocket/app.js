28637a2b3661e2b7816adf7f1d82678c52a0d3aa
L3ve <370157506@qq.com>
2016-04-08 15:58:05 +0800

    server  update

---
+++ nodeSocket/app.js
@@ -0,0 +1,43 @@
+var render = require('./src/render');
+var logger = require('koa-logger');
+var route = require('koa-route');
+var parse = require('co-body');
+var koa = require('koa');
+var app = koa();
+
+var posts = [];
+
+app.use(logger());
+
+
+app.use(route.get('/', list));
+app.use(route.get('/post/new', add));
+app.use(route.get('/post/:id', show));
+app.use(route.post('/post', create));
+
+function *list() {
+  this.body = yield render('list', { posts: posts });
+}
+
+function *add() {
+  this.body = yield render('new');
+}
+
+function *show(id) {
+  var post = posts[id];
+  if (!post) this.throw(404, 'invalid post id');
+  this.body = yield render('show', { post: post });
+}
+
+function *create() {
+  var post = yield parse(this);
+  var id = posts.push(post) - 1;
+  post.created_at = new Date;
+  post.id = id;
+  this.redirect('/');
+}
+
+// listen
+
+app.listen(3000);
+console.log('listening on port 3000');
