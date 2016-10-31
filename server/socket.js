3be35ac27ac7ae9e56ae410df11758dc992bad69
L3ve <370157506@qq.com>
2016-10-31 20:02:59 +0800

    改用nedb

--- server/socket.js
+++ server/socket.js
@@ -17,12 +17,14 @@ class Io {
             , fruits: ['apple', 'orange', 'pear']
             , infos: { name: 'nedb' }
         };
-
-        db.insert(doc, function (err, newDoc) {   // Callback is optional
-            // newDoc is the newly inserted document, including its _id
-            // newDoc has no key called notToBeSaved since its value was undefined
-            console.log(newDoc);
-        });
+        db.find({n:5},(err,res)=>{
+            console.log(res);
+        })
+        // db.insert(doc, function (err, newDoc) {   // Callback is optional
+        //     // newDoc is the newly inserted document, including its _id
+        //     // newDoc has no key called notToBeSaved since its value was undefined
+        //     console.log(newDoc);
+        // });
     }
     start() {
         io.on('connection', (client) => {
